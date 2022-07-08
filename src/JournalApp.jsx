6306b95e53b52d4cc463e751ba9c26f808e82821
unknown <andresfelipehc@ufps.edu.co>
2022-07-08 16:28:11 -0500

    definiendo rutas

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { AppRouter } from './router/AppRouter'
 
 export const JournalApp = () => {
   return (
     <div>
-        <h1>HOLA MUNDO</h1>
+        <AppRouter />
     </div>
   )
 }
