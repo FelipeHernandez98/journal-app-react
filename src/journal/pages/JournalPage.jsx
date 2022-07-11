3dab884615f43355e72e299e2b1423e317eaf319
unknown <andresfelipehc@ufps.edu.co>
2022-07-11 16:00:11 -0500

    creando el diseño del navbar

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+        <Typography>
+            Ex quis cupidatat non mollit et dolore. Eiusmod commodo labore aute dolore officia tempor. Quis amet non occaecat occaecat labore. Laboris cupidatat proident tempor esse aute sunt aute quis in eiusmod veniam. Elit qui irure ea in minim labore.
+        </Typography>
+
+        {/* Nothing selected */}
+    </JournalLayout>
+  )
+}
