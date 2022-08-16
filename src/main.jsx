ae4232063fc8b2b5a29942582586db6cf6315e17
unknown <andresfelipehc@ufps.edu.co>
2022-08-16 09:46:10 -0500

    aplicando redux y creando el primer slice de autenticación

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { JournalApp } from './JournalApp'
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './style.css'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
